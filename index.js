1724b72bb568b7f1a3a3b012602443f351a534f4
gsimpson@airmaster.com.au <gsimpson@airmaster.com.au>
2018-06-30 06:11:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // write your code below!
 
 var name = "Glen"
-var height = "176"
+var height = 176
 
 console.log("Name:")
 console.log(name)
 
 console.log("Height:")
-console.log(parseInt(height) + 1)
+console.log(height + 1)
